cfbb5304feaff5e759fcd74083f4eecd574760aa
fervin29 <113548563+fervin29@users.noreply.github.com>
2022-10-26 19:15:31 -0500

    practica

---
+++ clase 0/index.js
@@ -0,0 +1,15 @@
+function ejemplo(argument1){
+    if (typeof argument1 === "number"){
+        let i = 0
+        while ( i < argument1)
+        console.log("C is fun")
+        i++
+    }
+    else if ( typeof argument1 === undefined) {
+        console.log("Missing number")
+
+    }
+    
+    
+}
+
